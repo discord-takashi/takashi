6c81b1f8d4a598dc6290da9a9252b2abbe8f6b6e
7wf <7ywolf@gmail.com>
2020-03-07 16:15:48 -0300

    feat: setup discord client activity

--- src/modules/ready.module.ts
+++ src/modules/ready.module.ts
@@ -1,11 +1,26 @@
 import { Takashi } from '../core'
 
+import Topic from '../models/topic'
+
 /**
  * Outputs a message when the bot is ready.
  */
 export default function ready(takashi: Takashi) {
-    takashi.client.on('ready', () => {
+    async function setupActivity() {
+        takashi.client.user!.setActivity({
+            type: 'STREAMING',
+            url: 'https://twitch.tv/discord_takashi',
+            name: `${await Topic.countDocuments()} notification sources for ${
+                takashi.client.users.cache.size - 1 // not streaming for himself
+            } users`
+        })
+    }
+
+    takashi.client.once('ready', () => {
         const takashiName = takashi.client.user!.tag
         console.log(`${takashiName} is ready.`)
+
+        setupActivity()
+        setInterval(setupActivity, 5 * 60 * 1000)
     })
 }
