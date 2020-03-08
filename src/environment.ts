d8c4c00ce36869dffe2847e53536170bcd9414c5
7wf <7ywolf@gmail.com>
2020-03-07 22:58:42 -0300

    refactor: remove unused environment variable

--- src/environment.ts
+++ src/environment.ts
@@ -1,9 +1,4 @@
 /**
- * The port where the web server will listen in.
- */
-export const APP_PORT = process.env.APP_PORT || 3000
-
-/**
  * The Discord authorization token.
  */
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN
