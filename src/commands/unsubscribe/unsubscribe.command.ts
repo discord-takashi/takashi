c70cca7b31f2c44c76d45bbd0412f6cbed6e8a7d
tywolf <kanekivrau@gmail.com>
2020-03-08 21:08:54 -0300

    feat(command): allow users to unsubscribe from topics

---
+++ src/commands/unsubscribe/unsubscribe.command.ts
@@ -0,0 +1,37 @@
+import { Command, TakashiContext } from '../../core'
+
+import UnsubscribeService from './unsubscribe.service'
+
+/**
+ * The command for unsubscribing from topics.
+ */
+export default class UnsubscribeCommand extends Command<UnsubscribeService> {
+    /**
+     * Initializes the `unsubscribe` command.
+     */
+    public constructor() {
+        super('unsubscribe', UnsubscribeService)
+    }
+
+    /**
+     * Unsubscribes from a topic.
+     */
+    public async execute({ message, rawArguments }: TakashiContext): Promise<any> {
+        const targetTopic = await this.service.getTopicById(rawArguments.shift()!)
+
+        if(targetTopic) {
+            const unsubscribed = await this.service.removeSubscriptionFromTopic(
+                message.author.id,
+                targetTopic
+            )
+
+            return message.channel.send(
+                unsubscribed
+                    ? `Successfully unsubscribed from "${targetTopic.name}".`
+                    : `You're not subscribed to this topic.`
+            )
+        } else {
+            return message.channel.send(`The topic cannot not found.`)
+        }
+    }
+}
---
+++ src/commands/unsubscribe/unsubscribe.service.ts
@@ -0,0 +1,32 @@
+import Topic, { TopicDocument } from "../../models/topic";
+import User from "../../models/user";
+
+/**
+ * The service for the `unsubscribe` command.
+ */
+export default class UnsubscribeService {
+
+    /**
+     * Finds a topic by the ID.
+     */
+    public async getTopicById(id: string): Promise<TopicDocument| null> {
+        return Topic.findOne({ id })
+    }
+
+    /**
+     * Removes a user subscription from a topic.
+     */
+    public async removeSubscriptionFromTopic(userId: string, topic: TopicDocument): Promise<boolean> {
+        const user = await User.findOne({ id: userId })
+
+        if (user) {
+            if (!topic.subscribers.includes(user._id)) return false
+
+            await topic.update({ $pull: { subscribers: user._id } })
+            return true
+        }
+
+        return true
+    }
+
+}
