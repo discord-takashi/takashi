723e9cbc3aa6d046097614fb79d6e3be046f8f12
7wf <7ywolf@gmail.com>
2020-03-10 17:09:15 -0300

    feat: setup sentry

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+import * as sentry from '@sentry/node'
+
+// Load environment variables from `.env`
 require('dotenv/config')
+
+// Sentry Setup
+const dsn = process.env.SENTRY_DSN
+const environment = process.env.NODE_ENV
+
+if (dsn) sentry.init({ environment, dsn })
+
+// Runs the application
 require('./main')
