69d25e4b71016c971ea18e86539fc9864ae0c336
7wf <7ywolf@gmail.com>
2020-03-14 11:41:03 -0300

    feat(i18n): interface for languages

---
+++ src/core/i18n/language.ts
@@ -0,0 +1,22 @@
+/**
+ * Represents a language.
+ */
+export interface Language {
+    /**
+     * The code of the language.
+     */
+    code: string
+
+    /**
+     * The localized name of the language.
+     */
+    name: string
+
+    /**
+     * The translation strings.
+     */
+    strings: {
+        'default': (key: string) => string 
+        [key: string]: (...args: any) => string | string
+    }
+}
